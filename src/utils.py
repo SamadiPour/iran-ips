c6e46689bc1af63287741805cf54a72d5be5fd4e
SamadiPour <samadipoor2@gmail.com>
2021-12-07 17:08:09 +0330

    feat: add private IPs in utils for future configs

--- src/utils.py
+++ src/utils.py
@@ -65,3 +65,26 @@ def remove_subnet_ips(ip_list: list[ipaddress.IPv4Network]):
         # remove the subset_indexes from the list
         for x in sorted(subset_indexes, reverse=True):
             del ip_list[x]
+
+
+def get_private_ip_networks():
+    return [
+        ipaddress.ip_network("0.0.0.0/8"),
+        ipaddress.ip_network("10.0.0.0/8"),
+        ipaddress.ip_network("100.64.0.0/10"),
+        ipaddress.ip_network("127.0.0.0/8"),
+        ipaddress.ip_network("169.254.0.0/16"),
+        ipaddress.ip_network("172.16.0.0/12"),
+        ipaddress.ip_network("192.0.0.0/24"),
+        ipaddress.ip_network("192.0.2.0/24"),
+        ipaddress.ip_network("192.88.99.0/24"),
+        ipaddress.ip_network("192.168.0.0/16"),
+        ipaddress.ip_network("198.18.0.0/15"),
+        ipaddress.ip_network("198.51.100.0/24"),
+        ipaddress.ip_network("203.0.113.0/24"),
+        ipaddress.ip_network("224.0.0.0/3"),
+    ]
+
+
+def get_private_ip_networks_str():
+    return map(str, get_private_ip_networks())
